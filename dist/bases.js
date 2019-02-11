58cfaf036fa7ca20104dde94d77ea703f69ffb80
dronan <dronan@gmail.com>
2019-02-10 22:19:44 -0200

    primeiras aulas de ts: tipos de variavies, funcoes, arrays e comparacoes com o JS

---
+++ dist/bases.js
@@ -0,0 +1,20 @@
+// variaveis
+/*
+let tipoString: string
+tipoString = 'Nome';
+
+let tipoString = 'Nome'; // inferencia de tipo, aqui entra como string
+*/
+// tipos possiveis:
+/*
+let booleano:boolean;
+let numero:number;
+let texto:string;
+let naorecomendado:any // nao recomendado
+*/
+// arrays
+/*
+let players: number[] = [1,2,3]
+let players: Array<number> = [1,2,3]
+let players = [1,2,3]; // inferencia de tipo; aqui entra como number
+*/ 
