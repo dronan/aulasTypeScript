fcc3892fbb3853362c9bc0fc49633318e21697f8
dronan <dronan@gmail.com>
2019-02-16 11:35:47 -0200

    exemplo de uso de herança, extensão de caracteristicas

--- class.ts
+++ class.ts
@@ -9,3 +9,23 @@ class Spacecraft {
 
 let shipClassExample = new Spacecraft('hyperdrive');
 shipClassExample.jumpToHyperspace();
+
+
+class MillenniumFalcon extends Spacecraft {
+
+    constructor(){
+        super('hyperdrive'); // "new Spacecraft bla bla bla"
+    }
+
+    jumpToHyperspace(){
+        if (Math.random() > 0.5)
+            super.jumpToHyperspace() // chama o metodo da classe Spacecraft
+        else    
+            console.log(`Failed to jump to hyperspace`) // continua no metodo local
+    }
+
+}
+
+
+let falcon = new MillenniumFalcon();
+falcon.jumpToHyperspace();
--- dist/class.js
+++ dist/class.js
@@ -1,3 +1,16 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 var Spacecraft = /** @class */ (function () {
     function Spacecraft(propulsor) {
         this.propulsor = propulsor;
@@ -9,3 +22,18 @@ var Spacecraft = /** @class */ (function () {
 }());
 var shipClassExample = new Spacecraft('hyperdrive');
 shipClassExample.jumpToHyperspace();
+var MillenniumFalcon = /** @class */ (function (_super) {
+    __extends(MillenniumFalcon, _super);
+    function MillenniumFalcon() {
+        return _super.call(this, 'hyperdrive') || this;
+    }
+    MillenniumFalcon.prototype.jumpToHyperspace = function () {
+        if (Math.random() > 0.5)
+            _super.prototype.jumpToHyperspace.call(this); // chama o metodo da classe Spacecraft
+        else
+            console.log("Failed to jump to hyperspace"); // continua no metodo local
+    };
+    return MillenniumFalcon;
+}(Spacecraft));
+var falcon = new MillenniumFalcon();
+falcon.jumpToHyperspace();
